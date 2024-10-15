326dcf25be870081b61b5c57429f8e74ed4e6817
bença pai <148370307+jaodufrifai@users.noreply.github.com>
2024-10-15 16:17:42 -0300

    fsdfdsfgfdsffdsfdfgdfgfdsfdsf

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'programação',
-    'O que o Pyhton',
-    'O Python é uma linguagem de programação',
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual é a captal da França?',
+    'A captal da França é Paris'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
+    'O que é uma função',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
-    'Oi em inglês é Hi (RAI)',
+    'Oi em Inglês é HI (RAI)'
 )
 
 criaCartao(
     'Biologia',
-    'Quem é considerado o pai da genética',
+    'Quem é considerado o pai da genética?',
     'Gregor Mendel'
 )
 
 criaCartao(
-    'matemática',
+    'Matemática',
     'Qual é o valor de Pi?',
     '3,14'
 )
