27acbed2360e51a64caae26e861fdee943de425a
bença pai <148370307+jaodufrifai@users.noreply.github.com>
2024-11-05 15:17:36 -0300

    qwerty

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
-
-criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
-)
-
-criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Biologia',
+    'Quais são as diferenças fundamentais entre DNA e RNA e suas respectivas funções no organismo?',
+    'DNA armazena informações genéticas e é de fita dupla; RNA auxilia na síntese de proteínas e é de fita simples'
 )
 
 criaCartao(
     'Biologia',
-    'Quem é considerado o pai da genética?',
-    'Gregor Mendel'
+    'Como a hereditariedade influencia características físicas e predisposições a certas doenças?',
+    'Características físicas e predisposições a doenças são passadas geneticamente de pais para filhos'
 )
 
 criaCartao(
-    'Matemática',
-    'Qual é o valor de Pi?',
-    '3,14'
+    'Biologia',
+    'O que é engenharia genética e quais são algumas de suas aplicações atuais e potenciais futuras??',
+    'Manipulação do DNA para fins como produção de medicamentos e terapia gênica, com potencial futuro para curar doenças complexas.'
+)
+
+criaCartao(
+    'Biologia',
+    'Como as mutações genéticas ocorrem e quais são seus possíveis efeitos na saúde e no desenvolvimento?',
+    'Alterações no DNA que podem ocorrer espontaneamente ou devido a agentes mutagênicos, podendo causar doenças.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o papel dos genes e do ambiente na determinação do comportamento humano?',
+    'Ambos influenciam o comportamento humano, com os genes predispondo e o ambiente moldando esses comportamentos.'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são as implicações éticas das tecnologias de edição genética, como o CRISPR, na medicina e na sociedade?',
+    'Tecnologias como o CRISPR oferecem benefícios médicos, mas levantam questões éticas sobre o uso responsável e desigualdades.'
 )
